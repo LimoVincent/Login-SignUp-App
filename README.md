c5525dcf0446feb91d4b6643ec1b5d1630f30f8b
Limo Vincent <48591371+LimoVincent@users.noreply.github.com>
2020-04-13 17:35:07 +0300

    Form and Sign in folders with its directories, a better look:)

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 <h1>Login SignUp Form</h1>
 
 
-
+Target finnish date : ~30th May
 .....................
--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
 import './App.css';
+import Form from './Form/Form.js'
+import SignIn from './SignIn/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <h1>Dive in!!!!</h1>
-    </div>
+
+      <h1>Sign Up page</h1>
+      <Form/>
+
+      <br></br>
+      <h1> Log in page</h1>
+      <SignIn/>
+      
+          </div>
   );
 }
 
