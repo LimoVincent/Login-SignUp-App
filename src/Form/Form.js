f3a44998db4be83763fc919d890da69b436b9c86
Limo Vincent <48591371+LimoVincent@users.noreply.github.com>
2021-01-01 20:40:11 +0200

    added hooks to login.js for easy manipulation

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,33 +1,52 @@
-import React, { Component } from 'react'
-import './Form.css'
- class Form extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             firsName:'',
-             lastName : '',
-             userName: '',
-             email : '',
-             password : ''
-        }
-    }
-    
-    render() {
-        return (
-            <div>
-    <form className = 'signUpForm'>
-    First Name: <input type = 'text' placeholder = 'First Name ' firstName = {this.state.firsName} onChange ={e => this.setState({firstName : e.target.value})}></input> <br></br>
-    Last Name :  <input type = 'text'  placeholder = 'Last Name ' value = {this.state.lastName} onChange ={e => this.setState({lastName : e.target.value})}></input><br></br>
-    User Name :<input type = 'text' placeholder = 'User Name ' value = {this.state.userName} onChange ={e => this.setState({userName : e.target.value})}></input><br></br>
-    Email addr : <input type = 'email' placeholder = 'Email ' value = {this.state.email} onChange ={e => this.setState({email : e.target.value})}></input><br></br>
-    Password   :<input type = 'password'  placeholder = 'Password' value = {this.state.password} onChange ={e => this.setState({password : e.target.value})}></input><br></br>
+import React, { useState } from 'react'
+export default function Form() {
+  const [values, SetValues] = useState([
+    { firsName: '' },
+    { lastName: '' },
+    { userName: '' },
+    { email: '' },
+    { password: '' },
+  ])
 
-<button>Sign Up</button> <br></br>
-<button>Sign In Instead</button>
-                </form>
-            </div>
-        )
-    }
+  return
+  ;<div>
+    <form className='signUpForm'>
+      <label htmlFor='firstName'>First Name</label>
+      <input
+        type='text'
+        placeholder='First Name '
+        onChange={hadleChange}
+      ></input>
+      <label htmlFor='lastName'>Last Name</label>
+      <input
+        type='text'
+        placeholder='Last Name '
+        value={null}
+        onChange={handleChange}
+      ></input>
+      <label htmlFor='userName'>User Name :</label>
+      <input
+        type='text'
+        placeholder='User Name '
+        value={null}
+        onChange={handleChange}
+      ></input>
+      <label htmlFor='email'>Email addr :</label>
+      <input
+        type='email'
+        placeholder='Email '
+        value={null}
+        onChange={handleChange}
+      ></input>
+      Password :
+      <input
+        type='password'
+        placeholder='Password'
+        value={}
+        onChange={handleChange}
+      ></input>
+      <button>Sign Up</button> <br></br>
+      <button>Sign In Instead</button>
+    </form>
+  </div>
 }
-export default Form;
