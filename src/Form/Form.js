67cb308517e7f2158f79816727430101e61f4a7e
Limo Vincent <48591371+LimoVincent@users.noreply.github.com>
2021-01-01 20:45:39 +0200

    Update Form.js

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -8,45 +8,46 @@ export default function Form() {
     { password: '' },
   ])
 
-  return
-  ;<div>
-    <form className='signUpForm'>
-      <label htmlFor='firstName'>First Name</label>
-      <input
-        type='text'
-        placeholder='First Name '
-        onChange={hadleChange}
-      ></input>
-      <label htmlFor='lastName'>Last Name</label>
-      <input
-        type='text'
-        placeholder='Last Name '
-        value={null}
-        onChange={handleChange}
-      ></input>
-      <label htmlFor='userName'>User Name :</label>
-      <input
-        type='text'
-        placeholder='User Name '
-        value={null}
-        onChange={handleChange}
-      ></input>
-      <label htmlFor='email'>Email addr :</label>
-      <input
-        type='email'
-        placeholder='Email '
-        value={null}
-        onChange={handleChange}
-      ></input>
-      Password :
-      <input
-        type='password'
-        placeholder='Password'
-        value={}
-        onChange={handleChange}
-      ></input>
-      <button>Sign Up</button> <br></br>
-      <button>Sign In Instead</button>
-    </form>
-  </div>
+  return (
+    <div>
+      <form className='signUpForm'>
+        <label htmlFor='firstName'>First Name</label>
+        <input
+          type='text'
+          placeholder='First Name '
+          onChange={hadleChange}
+        ></input>
+        <label htmlFor='lastName'>Last Name</label>
+        <input
+          type='text'
+          placeholder='Last Name '
+          value={SetValues(values.firstName)}
+          onChange={handleChange}
+        ></input>
+        <label htmlFor='userName'>User Name :</label>
+        <input
+          type='text'
+          placeholder='User Name '
+          value={SetValues(values.userName)}
+          onChange={handleChange}
+        ></input>
+        <label htmlFor='email'>Email addr :</label>
+        <input
+          type='email'
+          placeholder='Email '
+          value={null}
+          onChange={handleChange}
+        ></input>
+        Password :
+        <input
+          type='password'
+          placeholder='Password'
+          value={}
+          onChange={handleChange}
+        ></input>
+        <button>Sign Up</button> <br></br>
+        <button>Sign In Instead</button>
+      </form>
+    </div>
+  )
 }
